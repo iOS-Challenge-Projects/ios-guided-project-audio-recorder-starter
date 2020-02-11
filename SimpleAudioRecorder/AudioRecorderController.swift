08da7e820e2064aa978f70bd568f56c85c1f6652
Paul Solt <PaulSolt@gmail.com>
2020-02-11 12:35:34 -0500

    Added the ability to play and pause audio

--- SimpleAudioRecorder/AudioRecorderController.swift
+++ SimpleAudioRecorder/AudioRecorderController.swift
@@ -56,6 +56,32 @@ class AudioRecorderController: UIViewController {
         audioPlayer = try? AVAudioPlayer(contentsOf: songURL)  // FIXME: use better error handling
     }
     
+    // What do I want to do?
+    // pause it
+    // volume
+    // restart the audio
+    // update the time/labels
+
+    var isPlaying: Bool {
+        audioPlayer?.isPlaying ?? false
+    }
+
+    func play() {
+        audioPlayer?.play()
+    }
+
+    func pause() {
+        audioPlayer?.pause()
+    }
+
+    func playPause() {
+        if isPlaying {
+            pause()
+        } else {
+            play()
+        }
+    }
+    
     
     // MARK: - Recording
     
@@ -64,8 +90,8 @@ class AudioRecorderController: UIViewController {
     // MARK: - Actions
     
     @IBAction func togglePlayback(_ sender: Any) {
-        audioPlayer?.play()
-	}
+        playPause()
+    }
     
     @IBAction func updateCurrentTime(_ sender: UISlider) {
         
